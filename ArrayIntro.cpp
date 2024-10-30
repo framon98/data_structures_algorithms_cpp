0bc6afecc186811e601bbd3b734d7fcbb6333fb7
Francisco Ramón Castro García <framon.98@outlook.com>
2024-10-29 22:18:53 -0400

    Add comment to file

--- ArrayIntro.cpp
+++ ArrayIntro.cpp
@@ -12,7 +12,7 @@ int main(){
     int a[4] = {1, 2, 3, 4}; /// Initializing the array
     // cout<<a[0]<<a[1]<<a[2]<<a[3]<<endl; /// Printing the values of the array
 
-    for(int idxi = 0; idxi < 4; idxi++){
+    for(int idxi = 0; idxi < 4; idxi++){ /// More efficient way of printing the array
         cout<<a[idxi]<<" ";
     }
 
