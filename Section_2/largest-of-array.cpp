6ed802e1efd1253d5085f56a9495825cd1ecad59
Francisco Ramón Castro García <framon.98@outlook.com>
2024-10-30 00:04:00 -0400

    Implementation fo smallest numebr in array

--- Section_2/largest-of-array.cpp
+++ Section_2/largest-of-array.cpp
@@ -10,13 +10,18 @@ int main(){
         std::cin >> arr[idxi];
     }
 
-    int largest_number = INT_MIN;
-
-    for (int idxi =0; idxi < number; idxi++){
+    int largest_number = INT_MIN; // You can also set this to the first element of the array
+    int smallest_number = INT_MAX;
+    // using alternative initialization you to use first instead of 0 element
+    for (int idxi =0; idxi < number; idxi++){ 
         if (arr[idxi] > largest_number){
             largest_number = arr[idxi];
+        }
+        if (arr[idxi] < smallest_number){
+            smallest_number = arr[idxi];
         }
     }
 
     std::cout << "Largest number is = " << largest_number << std::endl;
+    std::cout << "Smalelst value is = " << smallest_number << std::endl;
 }
