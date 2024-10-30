e22673dd96cedbb96ea65b3dd2cc68e871c1eec1
Francisco Ramón Castro García <framon.98@outlook.com>
2024-10-29 22:18:36 -0400

    Change how the array is printed

--- ArrayIntro.cpp
+++ ArrayIntro.cpp
@@ -10,7 +10,11 @@ int main(){
     cout<<a[0]<<" "<<a[4]<<endl; */
 
     int a[4] = {1, 2, 3, 4}; /// Initializing the array
-    cout<<a[0]<<a[1]<<a[2]<<a[3]<<endl;
+    // cout<<a[0]<<a[1]<<a[2]<<a[3]<<endl; /// Printing the values of the array
+
+    for(int idxi = 0; idxi < 4; idxi++){
+        cout<<a[idxi]<<" ";
+    }
 
     return 0;
 }
