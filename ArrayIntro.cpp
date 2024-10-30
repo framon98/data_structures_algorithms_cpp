54ef8bda920f48e6514fe08a3a5b926dae4939de
Francisco Ramón Castro García <framon.98@outlook.com>
2024-10-29 22:15:45 -0400

    Add gitignore, first arrayintro file

---
+++ ArrayIntro.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+/*     int a[10];
+    a[0] = 2;
+    a[4] = 5;
+
+    cout<<a[0]<<" "<<a[4]<<endl; */
+
+    int a[4] = {1, 2, 3, 4}; /// Initializing the array
+    cout<<a[0]<<a[1]<<a[2]<<a[3]<<endl;
+
+    return 0;
+}
