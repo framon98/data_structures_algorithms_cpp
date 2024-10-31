e3793f83fd84243f5ed86a5458a3aa6e2ce10e7a
Francisco Ramón Castro García <framon.98@outlook.com>
2024-10-31 01:00:30 -0400

    xor implementation of variable swap

--- Section_2/swap-2-numbers.cpp
+++ Section_2/swap-2-numbers.cpp
@@ -10,9 +10,15 @@ int main(){
     std::cout << "b = " << num_b << std::endl;
 
     //Using a temp variable is one way to do a number swap
-    int temp = num_a;
-    num_a = num_b;
-    num_b = temp;
+    // int temp = num_a;
+    // num_a = num_b;
+    // num_b = temp;
+
+    // If we use an xor or ^ we can do a swap with no temp variable
+    num_a = num_a ^ num_b;
+    num_b = num_a ^ num_b;
+    num_a = num_a ^ num_b;
+
     
     std::cout << "After Swapping" << std::endl;
     std::cout << "a = " << num_a << std::endl;
