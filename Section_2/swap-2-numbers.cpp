7eb7b690cc2997c731a8f52c60033d2d8606f5a8
Francisco Ramón Castro García <framon.98@outlook.com>
2024-10-31 01:01:39 -0400

    Using swap function

--- Section_2/swap-2-numbers.cpp
+++ Section_2/swap-2-numbers.cpp
@@ -15,9 +15,12 @@ int main(){
     // num_b = temp;
 
     // If we use an xor or ^ we can do a swap with no temp variable
-    num_a = num_a ^ num_b;
-    num_b = num_a ^ num_b;
-    num_a = num_a ^ num_b;
+    // num_a = num_a ^ num_b;
+    // num_b = num_a ^ num_b;
+    // num_a = num_a ^ num_b;
+
+    // From the standard library, swap does the same if given to variables
+    std::swap(num_a, num_b);
 
     
     std::cout << "After Swapping" << std::endl;
