f2c42636b400a2a3890ade9f6d9b8745ff4cf869
Francisco Ramón Castro García <framon.98@outlook.com>
2024-11-09 22:02:06 -0500

    Add fucntion that counts elements in a char array

--- Section_3/charArray.cpp
+++ Section_3/charArray.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 
+int length(char input[]){
+    int count = 0;
+    for (int idxi = 0; input[idxi] != '\0'; idxi++){
+        count++;
+    }
+    return count;
+}
+
 int main(){
 
     char name[100];
     std::cout << "Enter your name: ";
     std::cin >> name;
-    name[1] = '\0';
+/*     name[1] = '\0';
     name[3] = 'd';
-    name[4] = 'x';
+    name[4] = 'x'; */
     std::cout << "Your name is: " << name << std::endl;
 
+    std::cout << "Length is:" << length(name);
 
     return 0;
 }
