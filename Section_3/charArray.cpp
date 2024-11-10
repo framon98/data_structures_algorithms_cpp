061c4706c518a12a12934173d106cb191643608a
Francisco Ramón Castro García <framon.98@outlook.com>
2024-11-09 21:23:04 -0500

    Created new folder for section 3, add chararray example

---
+++ Section_3/charArray.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+
+    char name[100];
+    std::cout << "Enter your name: ";
+    std::cin >> name;
+    std::cout << "Your name is: " << name << std::endl;
+
+
+    return 0;
+}
