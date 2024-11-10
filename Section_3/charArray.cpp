2a3af3bc7fa5984908be4a66cc8fa8a22385dd04
Francisco Ramón Castro García <framon.98@outlook.com>
2024-11-09 21:51:18 -0500

    Change terminal char to show char array stopper

--- Section_3/charArray.cpp
+++ Section_3/charArray.cpp
@@ -5,6 +5,9 @@ int main(){
     char name[100];
     std::cout << "Enter your name: ";
     std::cin >> name;
+    name[1] = '\0';
+    name[3] = 'd';
+    name[4] = 'x';
     std::cout << "Your name is: " << name << std::endl;
 
 
