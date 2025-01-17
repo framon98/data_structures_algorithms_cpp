616366c2cf25a933d1cd3e0121d93ba88c4c3c5b
Francisco Ramon Castro Garcia <framon.98@outlook.com>
2025-01-16 19:32:29 -0600

    Add member functions to LL  to print, get head, tail and length

--- DSA_animated/Section_5/LL-Constructor.cpp
+++ DSA_animated/Section_5/LL-Constructor.cpp
@@ -24,4 +24,24 @@ class LinkedList {
             tail = newNode;
             length = 1;
         }
+
+        void printList(){
+            Node* temp = head;
+            while (temp != nullptr) {
+                std::cout << temp->value << std::endl;
+                temp = temp->next;
+            }
+        }
+
+        void getHead(){
+            std::cout << "Head: " << head->value << std::endl;
+        }
+        
+        void getTail(){
+            std::cout << "Tail: " << tail->value << std::endl;
+        }
+        
+        void getLength(){
+            std::cout << "Length: " << length << std::endl;
+        }
 };
