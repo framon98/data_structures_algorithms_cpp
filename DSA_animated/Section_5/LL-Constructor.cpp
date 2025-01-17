d2be94adf247b3467a523529bcb3c14bbc51c51e
Francisco Ramon Castro Garcia <framon.98@outlook.com>
2025-01-16 19:20:56 -0600

    Add member methods for Node and LinkedList class

--- DSA_animated/Section_5/LL-Constructor.cpp
+++ DSA_animated/Section_5/LL-Constructor.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 
 class Node {
+    public:
+        int value;
+        Node* next;
 
+        Node(int value){
+            this->value = value;
+            next = nullptr;
+        }
 };
 
 class LinkedList {
-
+    private:
+        Node* head;
+        Node* tail;
+        int length;
+    
+    public:
+        LinkedList(int value){
+            Node* newNode = new Node(value);
+            
+        }
 };
