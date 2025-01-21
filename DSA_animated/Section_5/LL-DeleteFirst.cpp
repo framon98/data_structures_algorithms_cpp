ec264c1eb57041db0553794fc90c847e92d0c2b3
Francisco Ramon Castro Garcia <framon.98@outlook.com>
2025-01-20 23:10:30 -0600

    Add initial delete first

---
+++ DSA_animated/Section_5/LL-DeleteFirst.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+
+class Node {
+    public:
+        int value;
+        Node* next;
+
+        Node(int value){
+            this->value = value;
+            next = nullptr;
+        }
+};
+
+class LinkedList {
+    private:
+        Node* head;
+        Node* tail;
+        int length;
+    
+    public:
+        LinkedList(int value){
+            Node* newNode = new Node(value);
+            head = newNode;
+            tail = newNode;
+            length = 1;
+        }
+
+        ~LinkedList(){
+            Node* temp = head;
+            while (head){
+                head = head->next;
+                delete temp;
+                temp = head;
+                
+            }
+        }
+
+        void append(int value){
+            Node* newNode = new Node(value);
+            if (head == nullptr){
+                head = newNode;
+                tail = newNode;
+            }else{
+
+                tail->next = newNode;
+                tail = newNode;
+            }
+            length++;
+        }
+
+        void deleteLast(){
+            if (tail == nullptr){
+                return;
+            }
+            Node* temp = head;
+            if (length == 1){
+                head = nullptr;
+                tail = nullptr;
+            }else{
+                Node* pre = head;
+                while (temp->next){
+                    pre = temp;
+                    temp = temp->next;
+                }
+                tail = pre;
+                tail->next = nullptr;
+            }            
+            delete temp;
+            length--;
+        }
+
+        void prepend(int value){
+            Node* newNode = new Node(value);
+
+            if (head == nullptr){
+                head = newNode;
+                tail = newNode;
+            }else{
+                newNode->next = head;
+                head = newNode;
+            }
+            length++;
+        }
+
+        void printList(){
+            Node* temp = head;
+            while (temp != nullptr) {
+                std::cout << temp->value << std::endl;
+                temp = temp->next;
+            }
+        }
+
+        void getHead(){
+            std::cout << "Head: " << head->value << std::endl;
+        }
+        
+        void getTail(){
+            std::cout << "Tail: " << tail->value << std::endl;
+        }
+        
+        void getLength(){
+            std::cout << "Length: " << length << std::endl;
+        }
+};
+
+int main(){
+
+    LinkedList* myLinkedList = new LinkedList(2);
+
+
+
+}
