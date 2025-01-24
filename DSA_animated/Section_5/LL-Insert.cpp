15a5d4504bc0dc616ee66cad56aa67aff1c574a7
Francisco Ramon Castro Garcia <framon.98@outlook.com>
2025-01-23 19:22:09 -0600

    Add test case

--- DSA_animated/Section_5/LL-Insert.cpp
+++ DSA_animated/Section_5/LL-Insert.cpp
@@ -159,12 +159,13 @@ class LinkedList {
 
 int main(){
 
-    LinkedList* myLinkedList = new LinkedList(11);
-    myLinkedList->append(3);
-    myLinkedList->append(23);
-    myLinkedList->append(7);
+    LinkedList* myLinkedList = new LinkedList(0);
+    myLinkedList->append(2);
+    // myLinkedList->append(23);
+    // myLinkedList->append(7);
 
-    myLinkedList->set(1, 4);
+    // myLinkedList->set(1, 4);
+    myLinkedList->insert(1, 1);
 
     myLinkedList->printList();
 
