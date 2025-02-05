bef42a0c48d8e86bdbcf420e733a22abfc634999
Francisco Ramon Castro Garcia <framon.98@outlook.com>
2025-02-04 20:40:16 -0600

    Add body of DLL to add append method

---
+++ DSA_animated/Section_8/DLL-Append.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+class Node {
+    public:
+        int value;
+        Node* next;
+        Node* prev;
+
+        Node(int value){
+            this->value = value;
+            next = nullptr;
+            prev = nullptr;
+        }
+};
+
+class DoublyLinkedList {
+    private:
+        Node* head;
+        Node* tail;
+        int length;
+
+    public:
+        DoublyLinkedList(int value){
+            Node* newNode = new Node(value);
+            head = newNode;
+            tail = newNode;
+            length = 1;
+
+        }
+
+        void printList(){
+            Node* temp = head;
+            while (temp)
+            {
+                std::cout << temp->value << std::endl;
+                temp = temp->next;
+            }
+        }
+
+        void getHead(){
+            std::cout << "Head: " << head->value << std::endl;
+        }
+
+        
+        void getTail(){
+            std::cout << "Tail: " << tail->value << std::endl;
+        }
+
+        void getLength() {
+            std::cout << "Length: " << length << std::endl;
+        }
+};
+
+int main(){
+    DoublyLinkedList* myDLL = new DoublyLinkedList(7);
+    myDLL->printList();
+}
