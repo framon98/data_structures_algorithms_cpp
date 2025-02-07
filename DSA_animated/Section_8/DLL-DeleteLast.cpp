882763f94a57d79293d6abe3e5f68f5ff0e93883
Francisco Ramon Castro Garcia <framon.98@outlook.com>
2025-02-06 19:57:25 -0600

    ADd baseline for delete last implementation

---
+++ DSA_animated/Section_8/DLL-DeleteLast.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+class Node {
+    public:
+        int value;
+        Node* next;
+        Node* prev;
+
+        Node(int value){
+            this->value = value;
+            next = nullptr;
+            prev = nullptr;
+        }
+};
+
+class DoublyLinkedList {
+    private:
+        Node* head;
+        Node* tail;
+        int length;
+
+    public:
+        DoublyLinkedList(int value){
+            Node* newNode = new Node(value);
+            head = newNode;
+            tail = newNode;
+            length = 1;
+
+        }
+
+        void printList(){
+            Node* temp = head;
+            while (temp)
+            {
+                std::cout << temp->value << std::endl;
+                temp = temp->next;
+            }
+        }
+
+        void getHead(){
+            std::cout << "Head: " << head->value << std::endl;
+        }
+
+        
+        void getTail(){
+            std::cout << "Tail: " << tail->value << std::endl;
+        }
+
+        void getLength() {
+            std::cout << "Length: " << length << std::endl;
+        }
+
+        void append(int value){
+            Node* newNode = new Node(value);
+            if (length = 0){
+                head = newNode;
+                tail = newNode;
+            }else{
+                tail->next = newNode;
+                newNode->prev = tail;
+                tail = newNode;
+            }
+            length++;
+        }
+};
+
+int main(){
+    DoublyLinkedList* myDLL = new DoublyLinkedList(1);
+    myDLL->printList();
+    myDLL->append(2);
+    myDLL->printList();
+
+}
