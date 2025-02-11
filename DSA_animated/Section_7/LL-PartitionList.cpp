5200d37d424513eef3c5b9a1942f76161014bd93
Francisco Ramon Castro Garcia <framon.98@outlook.com>
2025-02-10 22:12:45 -0600

    Remove previous attempt to cleanup code for future reference

--- DSA_animated/Section_7/LL-PartitionList.cpp
+++ DSA_animated/Section_7/LL-PartitionList.cpp
@@ -78,31 +78,6 @@ class LinkedList {
             }
             length++;
         }
-        
-        // void partitionList(int x){
-        //     Node* dummy1(0);
-        //     Node* dummy2(0);
-        //     Node* prev1 = dummy1;
-        //     Node* prev2 = dummy2;
-        //     Node* current = head;
-            
-        //     if (head == nullptr) return;
-        //     while (current != nullptr){
-        //         if (current->value < x){
-        //             prev1->next = current;
-        //             prev1 = current;
-        //         }else if (current->value <= x){
-        //             prev2->next = current;
-        //             prev2 = current;
-        //         }
-        //         current = current->next;
-        //     }
-        //     prev2->next = nullptr;
-        //     prev1->next = dummy2->next;
-        //     head = dummy1->next;
-            
-            
-        // }
 
         void partitionList(int x){
             if (head == nullptr) return;
