8677d6cf87cf24f758098a3758a7879336251437
Francisco Ramon Castro Garcia <framon.98@outlook.com>
2025-02-12 00:21:40 -0600

    Add constructor for the stack

--- DSA_animated/Section_11/Stack-Constructor.cpp
+++ DSA_animated/Section_11/Stack-Constructor.cpp
@@ -4,4 +4,22 @@ class Node{
     public:
         int value;
         Node* next;
+
+        Node(int value){
+            this->value = value;
+            next = nullptr;
+        }
+};  
+
+class Stack{
+    private: 
+        Node* top;
+        int height;
+
+    public:
+        Stack(int value){
+            Node* newNode = new Node(value);
+            top = newNode;
+            height = 1;
+        }
 };
