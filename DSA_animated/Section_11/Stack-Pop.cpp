d7b0c024b84a876a62a8779c4f38ed1f19637f03
Francisco Ramon Castro Garcia <framon.98@outlook.com>
2025-02-14 01:07:03 -0600

    Add implementation for pop method

---
+++ DSA_animated/Section_11/Stack-Pop.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+class Node{
+    public:
+        int value;
+        Node* next;
+
+        Node(int value){
+            this->value = value;
+            next = nullptr;
+        }
+};  
+
+class Stack{
+    private: 
+        Node* top;
+        int height;
+
+    public:
+        Stack(int value){
+            Node* newNode = new Node(value);
+            top = newNode;
+            height = 1;
+        }
+
+        void printStack() {
+            Node* temp = top;
+            while(temp) {
+                std::cout << temp->value << std::endl;
+                temp = temp->next;
+            }
+        }
+
+        void getTop() {
+            std::cout << "Top: " << top->value << std::endl;
+        }
+
+        void getHeight() {
+            std::cout << "Height: " << height << std::endl;
+        }
+
+        void push(int value){
+            Node* newNode = new Node(value);
+            newNode->next = top;
+            top = newNode;
+            height++;
+        }
+
+        int pop(){
+            if (height == 0) return INT_MIN;
+
+            Node* temp = top;
+            int poppedValue = top->value;
+            top = top->next;
+            delete temp;
+            height--;
+
+            return poppedValue;
+        }
+};
+
+int main(){
+
+    Stack* myStack = new Stack(2);
+
+    myStack->push(1);
+    myStack->push(0);
+
+    myStack->getTop();
+    myStack->getHeight();
+    myStack->printStack();
+
+    return 0;
+}    
