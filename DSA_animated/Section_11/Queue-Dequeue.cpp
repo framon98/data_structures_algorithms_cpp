f00c81b0936e98b63342b0001154dbb0b526d73a
Francisco Ramon Castro Garcia <framon.98@outlook.com>
2025-02-17 19:33:32 -0600

    Add base code for dequeue

---
+++ DSA_animated/Section_11/Queue-Dequeue.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+class Node {
+    public:
+        int value;
+        Node* next;
+
+        Node(int value) {
+            this->value = value;
+            next = nullptr;
+        }
+};
+
+class Queue {
+    private:
+        Node* first;
+        Node* last;
+        int length;
+    public:
+        Queue(int value){
+            Node* newNode = new Node(value);
+            first = newNode;
+            last = newNode;
+            length = 1;
+        }
+
+        void printQueue() {
+            Node* temp = first;
+            while(temp) {
+                std::cout << temp->value << std::endl;
+                temp = temp->next;
+            }
+        }
+
+        void getFirst() {
+            std::cout << "First: " << first->value << std::endl;
+        }
+
+        void getLast() {
+            std::cout << "Last: " << last->value << std::endl;
+        }
+
+        void getLength() {
+            std::cout << "Length: " << length << std::endl;
+        }
+
+        void enqueue(int value){
+            Node* newNode = new Node(value);
+            if (length == 0){
+                first = newNode;
+                last = newNode;
+            }else{
+                last->next = newNode;
+                last = newNode;
+            }
+            length++;
+        }
+};
+
+int main(){
+    Queue* myQueue = new Queue(1);
+    myQueue->printQueue();
+
+    myQueue->enqueue(2);
+    // myQueue->getFirst();
+
+    myQueue->printQueue();
+
+}
