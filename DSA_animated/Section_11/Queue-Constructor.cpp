2c13f1df0de2c6ab50e859d3de35e89ead2b06ea
Francisco Ramon Castro Garcia <framon.98@outlook.com>
2025-02-17 19:28:32 -0600

    Add constructor for queue

--- DSA_animated/Section_11/Queue-Constructor.cpp
+++ DSA_animated/Section_11/Queue-Constructor.cpp
@@ -10,3 +10,46 @@ class Node {
             next = nullptr;
         }
 };
+
+class Queue {
+    private:
+        Node* first;
+        Node* last;
+        int length;
+    public:
+        Queue(int value){
+            Node* newNode = new Node(value);
+            first = newNode;
+            last = newNode;
+            length = 1;
+        }
+
+        void printQueue() {
+            Node* temp = first;
+            while(temp) {
+                std::cout << temp->value << std::endl;
+                temp = temp->next;
+            }
+        }
+
+        void getFirst() {
+            std::cout << "First: " << first->value << std::endl;
+        }
+
+        void getLast() {
+            std::cout << "Last: " << last->value << std::endl;
+        }
+
+        void getLength() {
+            std::cout << "Length: " << length << std::endl;
+        }
+};
+
+int main(){
+    Queue* myQueue = new Queue(7);
+
+    myQueue->getFirst();
+
+    myQueue->printQueue();
+
+}
