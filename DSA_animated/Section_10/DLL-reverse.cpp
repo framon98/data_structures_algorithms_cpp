9211c90f988b4465013a601b81a80e99a984d9e9
Francisco Ramon Castro Garcia <framon.98@outlook.com>
2025-02-21 08:17:24 -0600

    Add solution to reversing a DLL

---
+++ DSA_animated/Section_10/DLL-reverse.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+
+class Node {
+    public:
+        int value;
+        Node* next;
+        Node* prev;
+
+        Node(int value){
+            this->value = value;
+            next = nullptr;
+            prev = nullptr;
+        }
+};
+
+class DoublyLinkedList {
+    private:
+        Node* head;
+        Node* tail;
+        int length;
+
+    public:
+        DoublyLinkedList(int value){
+            Node* newNode = new Node(value);
+            head = newNode;
+            tail = newNode;
+            length = 1;
+
+        }
+
+        void printList(){
+            Node* temp = head;
+            while (temp)
+            {
+                std::cout << temp->value << std::endl;
+                temp = temp->next;
+            }
+        }
+
+        void getHead(){
+            std::cout << "Head: " << head->value << std::endl;
+        }
+
+        
+        void getTail(){
+            std::cout << "Tail: " << tail->value << std::endl;
+        }
+
+        void getLength() {
+            std::cout << "Length: " << length << std::endl;
+        }
+
+        void append(int value){
+            Node* newNode = new Node(value);
+            if (length == 0){
+                head = newNode;
+                tail = newNode;
+            }else{
+                tail->next = newNode;
+                newNode->prev = tail;
+                tail = newNode;
+            }
+            length++;
+        }
+
+        void swapFirstLast(){
+            if (length < 2) return;
+            Node* temp = new Node(head->value);
+            head->value = tail->value;
+            tail->value = temp->value;
+        }
+
+        void reverse(){
+            if (length == 0) return;
+            if (length == 1) return;
+
+            Node* curr = head;
+            Node* temp = nullptr;
+            while (curr) {
+                temp = curr->prev;
+                curr->prev = curr->next;
+
+                curr->next = temp;
+                curr = curr->prev;
+            }
+            temp = head;
+            head = tail;
+            tail = temp;
+        }
+};
+
+int main(){
+    DoublyLinkedList* myDLL = new DoublyLinkedList(1);
+    // myDLL->printList();
+    myDLL->append(2);
+    myDLL->append(3);
+    myDLL->append(4);
+    myDLL->append(5);
+    std::cout << "Before Swap:" << std::endl;
+    myDLL->printList();
+    // myDLL->swapFirstLast();
+    myDLL->reverse();
+    std::cout << "After Swap:" << std::endl;
+    myDLL->printList();
+
+}
