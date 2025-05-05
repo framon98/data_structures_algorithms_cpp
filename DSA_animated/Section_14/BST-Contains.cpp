1181f798f31845a6ec8aff4d03bde2bb318253d7
Francisco Ramon Castro Garcia <framon.98@outlook.com>
2025-05-05 15:10:16 -0600

    Add initial base code from insert file

---
+++ DSA_animated/Section_14/BST-Contains.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+class Node {
+    public:
+        int value;
+        Node* left;
+        Node* right;
+
+        Node(int value){
+            this->value = value;
+            left = nullptr;
+            right = nullptr;
+        }
+};
+
+class BinarySearchTree { // Can be created to be empty to add the first node with insert instead
+    // This also can be done for LL by pointing head and tail to nullptr and length to 0
+    public: // This is changed to public in order to access the root node of the tree
+        Node* root;
+
+    public:
+        BinarySearchTree(){
+            root = nullptr;
+        }
+
+        bool insert(int value) {
+            Node* newNode = new Node(value);
+            if (root == nullptr){
+                root = newNode;
+                return true;
+            }
+            Node* temp = root;
+            while (true){
+                if (newNode->value == temp->value) return false;
+                if (newNode->value < temp->value) {
+                    if (temp->left == nullptr){
+                        temp->left = newNode;
+                        return true;
+                    }
+                    temp = temp->left;
+                }else{
+                    if (temp->right == nullptr){
+                        temp->right = newNode;
+                        return true;
+                    }
+                    temp = temp->right;
+                }
+            }
+        }
+};
+
+int main(){
+    BinarySearchTree* myBST = new BinarySearchTree();
+
+    myBST->insert(47);
+    myBST->insert(21);
+    myBST->insert(76);
+    myBST->insert(18);
+    myBST->insert(52);
+    myBST->insert(82);
+
+    myBST->insert(27);
+
+    // std::cout << "Root: " << myBST->root;
+    std::cout << myBST->root->left->right->value << std::endl;
+}
