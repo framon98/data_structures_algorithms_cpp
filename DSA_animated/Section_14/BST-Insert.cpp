d52f877b1061621ed0960879b497bf8c655e7731
Francisco Ramon Castro Garcia <framon.98@outlook.com>
2025-05-05 14:59:42 -0600

    Add insert function to file

--- DSA_animated/Section_14/BST-Insert.cpp
+++ DSA_animated/Section_14/BST-Insert.cpp
@@ -22,6 +22,31 @@ class BinarySearchTree { // Can be created to be empty to add the first node wit
         BinarySearchTree(){
             root = nullptr;
         }
+
+        bool insert(int value) {
+            Node* newNode = new Node(value);
+            if (root == nullptr){
+                root = newNode;
+                return true;
+            }
+            Node* temp = root;
+            while (true){
+                if (newNode->value == temp->value) return false;
+                if (newNode->value < temp->value) {
+                    if (temp->left == nullptr){
+                        temp->left = newNode;
+                        return true;
+                    }
+                    temp = temp->left;
+                }else{
+                    if (temp->right == nullptr){
+                        temp->right = newNode;
+                        return true;
+                    }
+                    temp = temp->right;
+                }
+            }
+        }
 };
 
 int main(){
