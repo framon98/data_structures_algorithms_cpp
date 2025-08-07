85d83eab39200d9231d5df35d2b777542db5cc23
Francisco Ramon Castro Garcia <framon.98@outlook.com>
2025-08-06 20:06:26 -0600

    Add print for the hashtable and a new comment for the constructor

--- DSA_animated/Section_16/HT-Constructor.cpp
+++ DSA_animated/Section_16/HT-Constructor.cpp
@@ -18,4 +18,19 @@ class HashTable {
         static const int SIZE = 7;
         Node* dataMap[SIZE];
         // this will hold pointers to other elements
+        //This does not has a constructor it only creates the address space we will use
+
+    public:
+        void printTable(){
+            for(int idx = 0; idx < SIZE; idx++){
+                std::cout << idx << ":" << std::endl;
+                if(dataMap[idx]){
+                    Node* temp = dataMap[idx];
+                    while(temp) {
+                        std::cout << "  {" << temp->key << ", " << temp->value << "}" << std::endl;
+                        temp = temp->next;
+                    }
+                }
+            }
+        }
 };
