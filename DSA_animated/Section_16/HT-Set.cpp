e5d144ecf2c3a17cbadbaf119f80037f63d99818
Francisco Ramon Castro Garcia <framon.98@outlook.com>
2025-10-05 11:08:04 -0600

    Add constructor in set function

---
+++ DSA_animated/Section_16/HT-Set.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+class Node {
+    public:
+        std::string key;
+        int value;
+        Node* next;
+
+        Node(std::string key, int value){
+            this->key = key;
+            this->value = value;
+            next = nullptr;
+        }
+};
+
+class HashTable {
+    private: //Private by default
+        static const int SIZE = 7;
+        Node* dataMap[SIZE];
+        // this will hold pointers to other elements
+        //This does not has a constructor it only creates the address space we will use
+
+    public:
+        void printTable(){
+            for(int idx = 0; idx < SIZE; idx++){
+                std::cout << idx << ":" << std::endl;
+                if(dataMap[idx]){
+                    Node* temp = dataMap[idx];
+                    while(temp) {
+                        std::cout << "  {" << temp->key << ", " << temp->value << "}" << std::endl;
+                        temp = temp->next;
+                    }
+                }
+            }
+        }
+};
+
+
+int main(){
+    HashTable* myHashTable = new HashTable();
+
+    myHashTable->printTable();
+}
