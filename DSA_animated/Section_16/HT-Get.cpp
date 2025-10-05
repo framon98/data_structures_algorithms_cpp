85e42aa1834131aed3e3fac01792954d4f5e2258
Francisco Ramon Castro Garcia <framon.98@outlook.com>
2025-10-05 11:22:15 -0600

    Add set implementation to get fil

---
+++ DSA_animated/Section_16/HT-Get.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+class Node {
+    public:
+        std::string key;
+        int value;
+        Node* next;
+
+        Node(std::string key, int value){
+            this->key = key;
+            this->value = value;
+            next = nullptr;
+        }
+};
+
+class HashTable {
+    private: //Private by default
+        static const int SIZE = 7;
+        Node* dataMap[SIZE];
+        // this will hold pointers to other elements
+        //This does not has a constructor it only creates the address space we will use
+
+    public:
+        void printTable(){
+            for(int idx = 0; idx < SIZE; idx++){
+                std::cout << idx << ":" << std::endl;
+                if(dataMap[idx]){
+                    Node* temp = dataMap[idx];
+                    while(temp) {
+                        std::cout << "  {" << temp->key << ", " << temp->value << "}" << std::endl;
+                        temp = temp->next;
+                    }
+                }
+            }
+        }
+        int hash(std::string key){
+            int hash = 0;
+            for (int idx = 0; idx < key.length(); idx++){
+                int asciiValue = int(key[idx]); // this turns strings into their ascii equivalents
+                hash = (hash + asciiValue * 23) % SIZE; //this allows the results to be more random
+            }
+            return hash;
+        }
+
+        void set(std::string key, int value){
+            int index = hash(key);
+            Node* newNode = new Node(key, value);
+            if (dataMap[index] == nullptr) { // No nodes at this address
+                dataMap[index] = newNode;
+            }else{ // There are nodes in the address already
+                Node* temp = dataMap[index];
+                while (temp->next != nullptr){
+                    temp = temp->next;
+                }
+                temp->next = newNode;
+            }
+        }
+};
+
+
+int main(){
+    HashTable* myHashTable = new HashTable();
+
+    myHashTable->set("nails", 100);
+    myHashTable->set("tile", 50);
+    myHashTable->set("lumber", 80);
+    myHashTable->set("bolts", 200);
+    myHashTable->set("screws", 140);
+
+    myHashTable->printTable();
+}
