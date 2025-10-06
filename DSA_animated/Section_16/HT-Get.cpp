533ef51c61d4d9ec5681726016b2ffba45fb02db
Francisco Ramon Castro Garcia <framon.98@outlook.com>
2025-10-05 18:13:27 -0600

    Update test cases

--- DSA_animated/Section_16/HT-Get.cpp
+++ DSA_animated/Section_16/HT-Get.cpp
@@ -77,8 +77,12 @@ int main(){
     myHashTable->set("nails", 100);
     myHashTable->set("tile", 50);
     myHashTable->set("lumber", 80);
-    myHashTable->set("bolts", 200);
-    myHashTable->set("screws", 140);
 
-    myHashTable->printTable();
+
+    // myHashTable->set("bolts", 200);
+    // myHashTable->set("screws", 140);
+
+    std::cout << "Lumber: " << myHashTable->get("lumber") << std::endl;
+    std::cout << "Bolts: " << myHashTable->get("bolts") << std::endl;
+    // myHashTable->printTable();
 }
