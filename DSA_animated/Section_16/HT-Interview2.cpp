f6bc68526021d792c4213601f2df58881ca20541
Francisco Ramon Castro Garcia <framon.98@outlook.com>
2025-10-05 19:19:49 -0600

    Change implementation to using an unordered map

--- DSA_animated/Section_16/HT-Interview2.cpp
+++ DSA_animated/Section_16/HT-Interview2.cpp
@@ -7,19 +7,25 @@
 
 #include <vector>
 #include <iostream>
+#include <unordered_map>
 
 bool itemInCommon(std::vector<int> vect1, std::vector<int> vect2){
-    for (auto idxi : vect1){
-        for (auto idxj : vect2){
-            if (idxi == idxj) return true;
-        }
+    std::unordered_map<int, bool> myMap;
+    for (auto idxi : vect1) {
+        myMap.insert({idxi, true});
     }
+
+    for (auto idxj : vect2) {
+        if (myMap[idxj]) return true;
+    }
+
     return false;
-} // This is not an efficient approach
+
+} // This is a more efficient approach
 
 int main(){
     std::vector<int> vect1 {1, 3, 5};
-    std::vector<int> vect2 {2, 4, 6};
+    std::vector<int> vect2 {2, 4, 5};
 
     std::cout << itemInCommon(vect1, vect2);
 }
