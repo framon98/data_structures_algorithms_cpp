9e5de01810adb3c24425f88691f10136b5754a0e
Francisco Ramon Castro Garcia <framon.98@outlook.com>
2025-10-05 19:02:35 -0600

    Add explanation and skeleton for the function

--- DSA_animated/Section_16/HT-Interview1.cpp
+++ DSA_animated/Section_16/HT-Interview1.cpp
@@ -1,1 +1,12 @@
 // Item in common 1
+// Given two lists with some values, check if they have a value in common
+// The simplest idea would be to lop through both of them, making this 
+// an O(n^2) problem for a loop within a loop
+// This can be like this
+
+#include <vector>
+#include <iostream>
+
+bool itemInCommon(std::vector<int> vect1, std::vector<int> vect1){
+    
+}
