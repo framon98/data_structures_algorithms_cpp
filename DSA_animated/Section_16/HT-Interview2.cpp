8962f06be4ac2a06f79954c9d817c4b34dff9fef
Francisco Ramon Castro Garcia <framon.98@outlook.com>
2025-10-05 19:17:18 -0600

    Add implementation file for better option

---
+++ DSA_animated/Section_16/HT-Interview2.cpp
@@ -0,0 +1,25 @@
+// Item in common 2
+// Given two lists with some values, check if they have a value in common
+// The simplest idea would be to lop through both of them, making this 
+// an O(n^2) problem for a loop within a loop
+// A better approach could be to create some sort of hash table to be able
+// to know in )(n) where the equal element is or if there is one
+
+#include <vector>
+#include <iostream>
+
+bool itemInCommon(std::vector<int> vect1, std::vector<int> vect2){
+    for (auto idxi : vect1){
+        for (auto idxj : vect2){
+            if (idxi == idxj) return true;
+        }
+    }
+    return false;
+} // This is not an efficient approach
+
+int main(){
+    std::vector<int> vect1 {1, 3, 5};
+    std::vector<int> vect2 {2, 4, 6};
+
+    std::cout << itemInCommon(vect1, vect2);
+}
